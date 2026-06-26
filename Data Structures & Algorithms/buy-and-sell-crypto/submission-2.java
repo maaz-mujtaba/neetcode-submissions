4ab09ae91ea72f5f26bd73cfcc014a5836c2db0f
Maaz Mujtaba <maaz32418@gmail.com>
2026-06-26 18:46:12 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int i = 0;
+        int j = 1;
+        int n = prices.length;
+        int buy = prices[0];
+
+        while(j < n)
+        {
+            int sell = prices[j] - buy;
+            if(sell < 0){
+                i++;
+                buy = prices[i];
+            }
+            profit = Math.max(sell,profit);
+            j++;
+        }
+        return profit==0?0:profit;
+    }
+}
