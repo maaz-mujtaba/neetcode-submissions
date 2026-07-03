a160b1b98e1d0992ca4243fcb2501d3301138b3a
Maaz Mujtaba <maaz32418@gmail.com>
2026-07-03 20:01:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,29 @@
+class MinStack {
+    Stack <Integer> stack;
+    Stack <Integer> min;
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(min.isEmpty() || val <= min.peek()){
+            min.push(val);
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if(stack.peek()==min.peek()) min.pop();
+
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
