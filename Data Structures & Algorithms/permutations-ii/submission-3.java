35011ad9721bfb506f8d49e366fda2ee61c72078
Maaz Mujtaba <maaz32418@gmail.com>
2026-09-01 23:09:49 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List <List<Integer>> list = new ArrayList<>();
+        help(0,nums,list);
+        return list;
+    }
+    void help(int index, int[]nums, List<List<Integer>> list)
+    {
+        if(index == nums.length)
+        {
+            List <Integer> arr = new ArrayList<>();
+            for(int i = 0; i<nums.length;i++)
+            {
+                arr.add(nums[i]);
+            }
+            list.add(new ArrayList<>(arr));
+            return;
+        }
+        HashSet <Integer> set = new HashSet<>();
+        for(int i = index; i<nums.length;i++)
+        {
+            if(set.contains(nums[i])) continue;
+            set.add(nums[i]);
+            swap(i,index,nums);
+            help(index+1,nums,list);
+            swap(i,index,nums);
+        }
+    }
+    void swap(int l, int r, int[]nums)
+    {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+}
